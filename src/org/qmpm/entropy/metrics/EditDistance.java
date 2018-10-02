834a89490497e77cff9b06bb80592c886edefd17
Christoffer <back@di.ku.dk>
2018-10-02 13:06:41 +0200

    comments

--- src/org/qmpm/entropy/metrics/EditDistance.java
+++ src/org/qmpm/entropy/metrics/EditDistance.java
@@ -1,3 +1,26 @@
+/*
+ * 	Eventropy - entropy estimation for XES event logs and other sequential data
+ * 
+ * 	Author: Christoffer Olling Back	<www.christofferback.com>
+ * 
+ * 	Copyright (C) 2018 University of Copenhagen 
+ * 
+ *	This file is part of Eventropy.
+ *
+ *	Eventropy is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Eventropy is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Eventropy.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.qmpm.entropy.metrics;
 
 import java.util.ArrayList;
